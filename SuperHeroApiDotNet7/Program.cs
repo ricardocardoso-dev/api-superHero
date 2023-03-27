31941f63ba6aef47225db91617ab3fdb7cc1b095
Ricardo Cardoso <ricardo.cardoso@gatec.com.br>
2023-03-27 01:24:06 -0300

    .NET7 WebAPI SuperHero

---
+++ SuperHeroApiDotNet7/Program.cs
@@ -0,0 +1,32 @@
+global using SuperHeroApiDotNet7.Models;
+using SuperHeroApiDotNet7.Services.SuperHeroService;
+using SuperHeroApiDotNet7.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
